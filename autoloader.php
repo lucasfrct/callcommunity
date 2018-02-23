90defc7dd6bfda187c7b9ac16b649a58ebb2fc8e
Lucas Costa <lucasfrct@gmail.com>
2018-02-23 20:54:29 -0300

    teste Nexmo Fail

--- autoloader.php
+++ autoloader.php
@@ -11,31 +11,33 @@
  * @param string $class The fully-qualified class name.
  * @return void
  */
-spl_autoload_register(function ($class) {
+spl_autoload_register ( function ( $class ) {
 
     // project-specific namespace prefix
-    $prefix = 'Foo\\Bar\\';
+    $prefix = ""; #'Foo\\Bar\\';
 
     // base directory for the namespace prefix
-    $base_dir = __DIR__ . '/src/';
+    $base_dir = __DIR__ . '/'; # . '/src/';
 
     // does the class use the namespace prefix?
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
+    $len = strlen ( $prefix );
+    if ( strncmp ( $prefix, $class, $len ) !== 0 ) {
         // no, move to the next registered autoloader
         return;
     }
 
     // get the relative class name
-    $relative_class = substr($class, $len);
+    $relative_class = substr ( $class, $len );
 
     // replace the namespace prefix with the base directory, replace namespace
     // separators with directory separators in the relative class name, append
     // with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $file = $base_dir . str_replace ( '\\', '/', $relative_class ) . '.php';
 
     // if the file exists, require it
-    if (file_exists($file)) {
+    if ( file_exists ( $file ) ) {
         require $file;
-    }
+    };
+
+    echo $file."<br>";
 });
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php
+
+include_once ( "autoloader.php");
+
+$client = new Nexmo\Client(new Nexmo\Client\Credentials\Basic("3cc5690d", "glClhm3x93AtIuaj"));  

fefb1306dfb2aeae70f7d65cb338767f05711f15
Lucas Costa <lucasfrct@gmail.com>
2018-03-02 11:28:01 -0300

    update Task css and html

--- vendor/calendar.js
+++ vendor/calendar.js
@@ -7,6 +7,8 @@ var $dom = { };
 		date: __resumeDay,
 		mounth: __resumeMounth,
 		task: __task,
+		taskTitle: __taskTitle,
+		getTask: $( ".calendar-day li[tabindex], .calendar-task .close" ),
 	} );
 
 	function __resumeDay (  $date = "", $week = "" ) {
@@ -22,51 +24,62 @@ var $dom = { };
 		$( ".calendar-day li[tabindex="+$hour+"]").text ( $name );
 	};
 
+	function __taskTitle ( $title = "" ) {
+		$( ".calendar-task .calendar-task-header input" ).disabled = false;
+		$( ".calendar-task .calendar-task-header input" ).val ( $title );
+	}
+
 } ) ( );
 
 
-alert ( $Date ( ).week ); 
-
-$dom.date ( "27", "ter" );
-$dom.mounth ( "março" );
+var $month = [
+	"janeiro", 
+	"fevereiro", 
+	"março", 
+	"abril", 
+	"maio", 
+	"junho", 
+	"julho", 
+	"agosto", 
+	"setembro", 
+	"outubro", 
+	"novembro", 
+	"dezembro", 
+];
 
 var $tasks = [ 
-	{ hour: 8, name: "Tarefa 1", },
-	{ hour: 12, name: "Tarefa 2", },
-	{ hour: 16, name: "Tarefa 3", },
-	{ hour: 17, name: "Tarefa 4", },
+	//{ hour: 8, name: "Tarefa 1", date:"2018-03-01",  },
 ];
+
+var $currentDate = $Date ( "", [ ], $month );
+
+
+$dom.date ( $currentDate.day, $currentDate.week );
+$dom.mounth ( $currentDate.Month );
 
 $tasks.forEach ( function ( $task ) {
 	$dom.task ( $task.hour , $task.name );
 } );
 
-
 openTask ( );
 
 
-
-
-
-
-
-
-function openCalendarDays ( ) {
-	alert ("Open Calendar Days");
-}
-
-function openCalendarMonths ( ) {
-	alert( "Open Calendar Months");
-}
-
 function openTask ( ) {
-	$( ".calendar-day li[tabindex]" ).on ( "click", __getTask );
+	
+	$dom.getTask.on ( "click", __getTask );
 	
 	function __getTask ( ) {
-
+		
 		var $hour = $( this ). attr ( "tabindex" );
-		alert( "Open Calendar Task: "+$hour );
 
 	};
 	
-}
+};
+
+function openCalendarDays ( ) {
+	console.log ("Open Calendar Days");
+};
+
+function openCalendarMonths ( ) {
+	console.log ( "Open Calendar Months");
+};
